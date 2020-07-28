1011a49e84dd90372577518599f6ecd03f218e52
password <rashaunwarner@yahoo.com>
2020-07-27 23:34:32 -0400

    added functional login and signup with aws amply cognito

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+import { Authenticator } from 'aws-amplify-react-native';
+
+Amplify.configure(config)
+
+function Home (props:any) {
+  if(props.authState === 'signedIn') return <Text>Home</Text>
+  else return <Text>Please Login</Text>
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Authenticator usernameAttributes="email">
+        <Home />
+      </Authenticator>
       <StatusBar style="auto" />
     </View>
   );
