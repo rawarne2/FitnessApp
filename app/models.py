5b40f89df1d699a8e4ffd86251382164fa8eb644
Rashaun Warner <rashaunwarner@yahoo.com>
2020-10-23 11:50:32 -0400

    added more fields to user and return user when creating

--- app/models.py
+++ app/models.py
@@ -2,15 +2,23 @@ from sqlalchemy import Column, Integer, String, DateTime
 from datetime import datetime
 from database import Base, db_session
 
+
 class User(Base):
     __tablename__ = 'users'
     name = 'User'
-    id = Column(Integer, primary_key=True)
+    sub = Column(String, primary_key=True)
     email_address = Column(String)
+    first_name = Column(String)
+    last_name = Column(String)
     last_login = Column(DateTime, default=datetime.now())
+    height = Column(Integer)
+    weight = Column(Integer)
 
-    def create(self, email_address: String):
-        new_user = User(email_address=email_address, last_login=datetime.now())
+    def create(self, email_address: String, first_name: String, last_name: String, sub: String, height: Integer,
+               weight: Integer):
+        new_user = User(email_address=email_address, first_name=first_name, last_name=last_name, sub=sub, height=height,
+                        weight=weight,
+                        last_login=datetime.now())
         db_session.add(new_user)
         db_session.commit()
         db_session.close()
--- app/schema.py
+++ app/schema.py
@@ -3,7 +3,7 @@ from __future__ import division
 from __future__ import print_function
 
 import graphene
-from graphene import Schema, String, List, Field, ID, DateTime, ObjectType
+from graphene import Schema, String, List, Field, ID, DateTime, ObjectType, Int
 # from graphql.type.definition import GraphQLResolveInfo
 from tornado.escape import to_unicode
 from datetime import datetime
@@ -19,9 +19,13 @@ class User(SQLAlchemyObjectType):
 
 
 class UserType(ObjectType):
+    sub = Int()
     email_address = String(required=True)
     last_login = DateTime()
-    id = ID()
+    height = Int()
+    weight = Int()
+    first_name = String()
+    last_name = String()
 
 
 class Query(ObjectType):
@@ -29,30 +33,40 @@ class Query(ObjectType):
 
     all_users = SQLAlchemyConnectionField(User.connection)
 
-    user = Field(User, email_address=String(required=True))
+    user = Field(User, sub=String(required=True))  # change to use sub
 
     @staticmethod
-    def resolve_user(self, info, email_address):
+    def resolve_user(self, info, sub):
         query = User.get_query(info)
 
-        return query.filter(UserModel.email_address == email_address).first()
+        return query.filter(UserModel.sub == sub).first()
 
 
 class CreateUser(graphene.Mutation):
     class Arguments:
-        email_address = graphene.String()
-        # last_login = graphene.DateTime()
-        # id = graphene.ID()
+        sub = String()
+        email_address = String()
+        first_name = String()
+        last_name = String()
+        height = Int()
+        weight = Int()
 
-    user = Field(lambda: UserType)
+    user = Field(User, email_address=String(required=True))  # change to use sub
 
     @staticmethod
-    def mutate(self, info, email_address):
+    def resolve_user(self, info, sub):
+        query = User.get_query(info)
+
+        return query.filter(UserModel.sub == sub).first()
+
+    @staticmethod
+    def mutate(self, info, sub, email_address, first_name, last_name, height, weight):
         # if info.context.get('is_admin'):
         #     email_address = email_address.upper()
         # user = UserType(email_address=email_address, last_login=datetime.now())
         # return CreateUser(user=user)
-        return CreateUser(user=UserModel.create(self, email_address=email_address))
+        return CreateUser(user=UserModel.create(self, sub=sub, email_address=email_address, first_name=first_name,
+                                                last_name=last_name, height=height, weight=weight))
 
 
 class Mutation(ObjectType):
